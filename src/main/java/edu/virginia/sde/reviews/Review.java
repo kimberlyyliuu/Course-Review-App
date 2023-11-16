32a478be6846013e2dfbf0773f76ebe43fb8b1cb
kimberlyyliuu <kimberlyliu722@gmail.com>
2023-11-15 22:58:26 -0500

    Filled Review.java

--- src/main/java/edu/virginia/sde/reviews/Review.java
+++ src/main/java/edu/virginia/sde/reviews/Review.java
@@ -1,14 +1,39 @@
 package edu.virginia.sde.reviews;
 
+import java.util.Date;
+
 public class Review {
     private final String user;
-    private final String courseName;
+    private String courseName;
+    private double rating;
+    private String comment;
+    private Date timestamp;
 
-
-    public Review(String user, String courseName){
+    public Review(String user, String courseName, double rating, String comment) {
         this.user = user;
         this.courseName = courseName;
+        this.rating = rating;
+        this.comment = comment;
+        this.timestamp = new Date(); // Set the timestamp to the current date and time
     }
 
+    public String getCourseName() {
+        return courseName;
+    }
 
+    public String getUser() {
+        return user;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
 }
