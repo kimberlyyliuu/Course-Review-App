066ce611c7599ec887f07f274669ac6ab795d7dd
miaar <miaark952@gmail.com>
2023-11-20 20:04:32 -0500

    Basic outline of LoginController

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,10 +1,70 @@
 package edu.virginia.sde.reviews;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 public class LoginController {
 
-    //create new user
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button newUserButton;
 
-    //login--need validation of existing user in database
+    @FXML
+    private void initialize(){
+        loginButton.setOnAction(event -> handleLogin());
+        newUserButton.setOnAction(event -> handleCreateNewUser());
+    }
 
-    //need to create user doesn't exist exception
+    private void handleLogin(){
+        var username = usernameField.getText();
+        var password = passwordField.getText();
+        var userExists = validateUser(username, password);
+
+        if(userExists){
+            openCourseReviewScene();
+        }
+        else{
+            //error message shown about invalid username or password or create new user
+        }
+    }
+
+    private void handleCreateNewUser(){
+        var username = usernameField.getText();
+        var password = passwordField.getText();
+        var usernameExists = checkUserExists(username);
+
+        if(usernameExists){
+            //error message about username already existing
+        }
+        else{
+            addUserToDatabase(username, password);
+            openCourseReviewScene();
+        }
+
+    }
+
+    private boolean validateUser(String username, String password){
+
+    }
+
+    private boolean checkUserExists(String username){
+
+    }
+
+    private void addUserToDatabase(String username, String password){
+
+    }
+
+    private void openCourseReviewScene(){
+      var stage = (Stage) loginButton.getScene().getWindow();
+      stage.close();
+      //idk
+    }
+
 }
