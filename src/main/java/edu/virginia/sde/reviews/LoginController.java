3a05b60c889aedb2ad0cb1e5f959b3eab08f847b
miaar <miaark952@gmail.com>
2023-11-20 19:26:55 -0500

    Skeleton graphic of log-in screen

---
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -0,0 +1,10 @@
+package edu.virginia.sde.reviews;
+
+public class LoginController {
+
+    //create new user
+
+    //login--need validation of existing user in database
+
+    //need to create user doesn't exist exception
+}
