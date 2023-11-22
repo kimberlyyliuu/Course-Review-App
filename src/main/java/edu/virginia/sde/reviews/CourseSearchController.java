f34213e8582d71465f26c18777120ff3bedf379e
Ashley <mqx4vk@virginia.edu>
2023-11-22 14:06:44 -0500

    Added an Add Course Button

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -3,11 +3,10 @@ package edu.virginia.sde.reviews;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-
-import java.lang.reflect.Field;
+import javafx.collections.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseSearchController {
     @FXML
@@ -28,8 +27,38 @@ public class CourseSearchController {
     @FXML
     private Button exitButton;
     @FXML
+    private Button addCourse;
+    @FXML
     protected void courseSearchInitialize(){
+        loadCourses();
         exitButton.setOnAction(event -> Platform.exit());
+        addCourse.setOnAction(event -> {
+            try {
+                addCourse();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+    private void loadCourses() {
+        try {
+            DatabaseDriver dbDriver = new DatabaseDriver("course_app.sqlite");
+            dbDriver.connect();
+            dbDriver.createTables();
+            List<Course> courses = new ArrayList<>();
+            courses = dbDriver.getAllCourses();
+            ObservableList<Course> observableCourseList = FXCollections.observableList(courses);
+            ListView<Course> courselistView = new ListView<>(observableCourseList);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void addCourse() throws SQLException {
+        DatabaseDriver dbDriver = new DatabaseDriver("course_app.sqlite");
+        dbDriver.connect();
+        dbDriver.createTables();
+
     }
 }
 
