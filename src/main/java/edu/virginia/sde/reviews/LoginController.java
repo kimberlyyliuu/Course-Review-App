b80ddc931efde1cada95d56e06a1f319107b0975
Ashley <mqx4vk@virginia.edu>
2023-11-21 23:26:32 -0500

    Made visual edits and added helpful fields for error messages

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,10 +1,15 @@
 package edu.virginia.sde.reviews;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.*;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -18,9 +23,20 @@ public class LoginController {
     private Button loginButton;
     @FXML
     private Button newUserButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Label userNotExist;
+    @FXML
+    private Label userAlreadyExists;
+    @FXML
+    private Label tryAgain;
+    @FXML
+    private Label invalidPassword;
 
     @FXML
     private void initialize(){
+        exitButton.setOnAction(event -> Platform.exit());
         loginButton.setOnAction(event -> handleLogin());
         newUserButton.setOnAction(event -> {
             try {
@@ -42,13 +58,18 @@ public class LoginController {
         try{
             DatabaseDriver dbDriver = new DatabaseDriver("course_app.sqlite");
             dbDriver.connect();
+            dbDriver.createTables();        //create tables if not exists
+            dbDriver.commit();
 
-            if(dbDriver.checkUserPassword(username, password)){
+            if(!dbDriver.checkUserExists(username)){
+                userNotExist.setText("Username does not exist");
+            }
+            else if (dbDriver.checkUserPassword(username, password)){ //queries for matching pass and returns false otherwise
                 dbDriver.disconnect();
                 openCourseReviewScene();
             }
             else{
-                //display error message
+               invalidPassword.setText("Invalid Password");
             }
         } catch (SQLException e){
             e.printStackTrace();
@@ -60,29 +81,21 @@ public class LoginController {
      * If not, the new user will be created successfully, and then the course review scene will be shown.
      * @throws SQLException
      */
+    //This was corrected and now new user is on another scene
     private void handleCreateNewUser() throws SQLException {
-        var username = usernameField.getText();
-        var password = passwordField.getText();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("New-User.fxml"));
+            Parent root = loader.load();
+            // Create a new scene
+            Scene newScene = new Scene(root);
+            // Stage and new scene for new user
+            Stage stage = (Stage) newUserButton.getScene().getWindow();
+            stage.setScene(newScene);
 
-        try{
-            DatabaseDriver dbDriver = new DatabaseDriver("course_app.sqlite");
-            dbDriver.connect();
-
-            if(dbDriver.checkUserExists(username)){
-                //error message
-            }
-            else{
-                User newUser = new User(username, password);
-
-                dbDriver.addUser(newUser);
-                dbDriver.disconnect();
-
-                openCourseReviewScene();
-            }
-        } catch (SQLException e){
+            System.out.println("Stage set, switching now");
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
 
